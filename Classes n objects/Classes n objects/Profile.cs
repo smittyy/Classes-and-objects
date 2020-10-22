f3565627ccdd29a39f11090dbd3347d1e3a92c2d
smittyy <57191850+smittyy@users.noreply.github.com>
2020-10-22 09:46:52 -0400

    Add files via upload

---
+++ Classes n objects/Classes n objects/Profile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DatingProfile
+{
+    class Profile
+    {
+        private string firstname;
+        private string lastname;
+        private int age;
+        private string city;
+        private string country;
+        private string pronouns;
+        private string[] hobbies;
+        private string willWalkDogs;
+
+        public Profile(string firstname, string lastname, int age, string city = "", string country = "", string pronouns = "they/them", string willWalkDogs = "")
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.age = age;
+            this.city = city;
+            this.country = country;
+            this.pronouns = pronouns;
+            this.willWalkDogs = willWalkDogs;
+        }
+
+        public Profile()
+        { }
+
+
+        public string ViewProfile()
+        {
+            string bio = ($"Name: {firstname} {lastname}\nAge: {age}\nCity: {city}\nCountry: {country}\nGender: {pronouns}");
+
+
+            if (this.hobbies.Length != 0)
+            {
+                foreach (string s in this.hobbies)
+                {
+                    bio += $"\nHobbies: {s}";
+                }
+            }
+
+            return age < 18 ? "You are not of age to view this profile" : bio;
+        }
+
+        public void SetHobbies(string[] hobbies) => this.hobbies = hobbies;
+
+        public string MaleOrFemale(string s) => s == "MALE" ? "Male" : "Female";
+    }
+}
---
+++ Classes n objects/Classes n objects/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DatingProfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Object of Your Affection");
+
+            Console.Write("Please enter First Name:");
+            string firstName = Console.ReadLine();
+            while (String.IsNullOrWhiteSpace(firstName))
+            {
+                Console.Write("Sorry, didn't understand.  What is your First Name?: ");
+                firstName = Console.ReadLine();
+            }
+
+            Console.Write("Please enter Last Name:");
+            string lastName = Console.ReadLine();
+            while (String.IsNullOrWhiteSpace(lastName))
+            {
+                Console.Write("Sorry, didn't understand.  What is your Last Name?: ");
+                firstName = Console.ReadLine();
+            }
+
+            Console.Write("Please enter your Age:");
+            string age = Console.ReadLine();
+            while (String.IsNullOrWhiteSpace(age))
+            {
+                Console.Write("Sorry, didn't understand.  What is your Age?: ");
+                firstName = Console.ReadLine();
+            }
+
+            int iAge;
+            while (!Int32.TryParse(age, out iAge))
+            {
+                Console.WriteLine("Please respond with a number for your age:");
+                Console.Write("Please enter your Age:");
+                age = Console.ReadLine();
+            }
+
+            Console.Write("Please enter your City:");
+            string city = Console.ReadLine();
+            while (String.IsNullOrWhiteSpace(city))
+            {
+                Console.Write("Sorry, didn't understand.  What is your City?: ");
+                city = Console.ReadLine();
+            }
+
+            Console.Write("Please enter your Country:");
+            string country = Console.ReadLine();
+            while (String.IsNullOrWhiteSpace(country))
+            {
+                Console.Write("Sorry, didn't understand.  What is your Country?: ");
+                country = Console.ReadLine();
+            }
+
+            Console.Write("Will you walk your dogs");
+            string willWalkDogs = Console.ReadLine();
+            while (String.IsNullOrWhiteSpace(willWalkDogs))
+            {
+                Console.Write("Sorry, didn't understand.  What is your Country?: ");
+                country = Console.ReadLine();
+            }
+
+            Console.Write("Please enter your Gender:");
+            string gender = Console.ReadLine();
+            while (String.IsNullOrWhiteSpace(gender))
+            {
+                Console.Write("Sorry, what is your Gender?: ");
+                gender = Console.ReadLine();
+            }
+
+            gender = gender.ToUpper();
+            while (gender != "MALE" && gender != "FEMALE")
+            {
+                Console.Write("Sorry, didn't understand.  What is your Gender?: ");
+                gender = Console.ReadLine().ToUpper();
+            }
+
+            var g = new Profile();
+            gender = g.MaleOrFemale(gender);
+
+            Console.Write("What are your hobbies?:");
+            string s = Console.ReadLine();
+            string[] hobbys = s.Split(' ').ToList().ToArray();
+
+            Profile sam = new Profile(firstName, lastName, iAge, city, country, gender);
+
+            if (hobbys != null)
+            {
+                sam.SetHobbies(hobbys);
+            }
+
+
+            Console.WriteLine(sam.ViewProfile());
+        }
+    }
+}
